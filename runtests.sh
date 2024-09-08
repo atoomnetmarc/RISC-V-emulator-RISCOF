e9d2717676646645e75bc2be171b4c0196cbbbc8
atoomnetmarc <atoomnetmarc@mail1.atoomnet.net>
2024-09-08 19:22:16 +0200

    More memory. More jobs. More timeout.

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-riscof run --no-browser --config=config.ini --suite=riscv-arch-test/riscv-test-suite/ --env=riscv-arch-test/riscv-test-suite/env
+riscof run --no-browser --config=config.ini --suite=riscv-arch-test/riscv-test-suite/ --env=riscv-arch-test/riscv-test-suite/env "$@"
+
--- rve/riscof_rve.py
+++ rve/riscof_rve.py
@@ -167,7 +167,7 @@ class rve(pluginTemplate):
 
       # once the make-targets are done and the makefile has been created, run all the targets in
       # parallel using the make command set above.
-      make.execute_all(self.work_dir)
+      make.execute_all(self.work_dir, timeout=900)
 
       # if target runs are not required then we simply exit as this point after running all
       # the makefile targets.
